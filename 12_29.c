d8f775c66cb404aac20cb2a10c5198464b7b23b5
JH010918 <qorn3ck@gmail.com>
2025-12-29 09:05:10 +0900

    2commit

--- 12_29.c
+++ 12_29.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int main(void)
+/*int main(void)
 {
-    int a;
-    a=10;
+    int a,b;
+    a=10,b=20;
     printf("a = %d\n",a);
+    printf("b=%d\n",b);
 
     return 0;
+}*/
+
+int main(void)
+{
+    
 }
