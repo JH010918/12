4749eb4bfaae3ac2425b4605f22f8508268cdb3c
JH010918 <qorn3ck@gmail.com>
2026-01-12 09:30:33 +0900

    1_12_1

--- 1_12.c
+++ 1_12.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello\n");
+    FILE *fp;
+
+    fp=fopen("a.txt","r");
+    if(fp==NULL){
+        printf("파일이 열리지 않았습니다.\n");
+        return 1;
+    }
+    printf("파일이 열렸습니다.\n");
+    fclose(fp);
+
+    return 0;
 }
