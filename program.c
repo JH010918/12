24dd2b3e13a75c1229e81606d5ebc064231f3ca3
JH010918 <qorn3ck@gmail.com>
2026-01-14 16:51:38 +0900

    1_14

--- program.c
+++ program.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define PI 3.1415926535897932
+
+void wheel_vel();
 
 int main(){
     int sel;
@@ -14,7 +17,7 @@ int main(){
     scanf("%d",&sel);
 
     if(sel==1){
-        printf("1");
+        wheel_vel();
     }
     else if(sel==2){
         printf("1");
@@ -36,4 +39,26 @@ int main(){
     }
 
     return 0;
+}
+
+void wheel_vel(){
+    printf("");
+    double r,W,err_l,err_r,L;
+    printf("바퀴 반지름[m] : ");
+    scanf("%lf",&r);
+    printf("트랙폭[m] : ");
+    scanf("%lf",&W);
+    printf("좌/우 바퀴 오차율(%%) : ");
+    scanf("%lf %lf",&err_l,&err_r);
+    printf("목표 주행 거리(m) : ");
+    scanf("%lf",&L);
+    if(r<=0||W<=0||err_l<0||err_r<0||err_l==err_r||L<=0) return;
+
+    double rad_l=r*(1+err_l/100),rad_r=r*(1+err_r/100);
+    double Rc=(W/2)*(rad_l+rad_r)/(rad_r-rad_l);
+    double angle_drift=L/Rc*180/PI;
+
+    printf("좌/우 바퀴 반경 : %lf m / %lf m\n",rad_l,rad_r);
+    printf("곡률반경 : %lf m\n",Rc);
+    printf("편류각 : %lf deg\n",angle_drift);
 }
